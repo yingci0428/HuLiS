2579a5d64afddd800e8f7011f51ab499f904af5e
yingci0428 <f510280@gmail.com>
2021-12-02 22:05:40 +0800

    git add .

--- hulis/js/hulis薪水.js
+++ hulis/js/hulis薪水.js
@@ -1,8 +1,13 @@
-let city_1 = document.getElementsByClassName("city_1");
-console.log("city_1");
-city_1.addEventListener("click", function () {
+const title = document.querySelector('.city_1');
 
+let svg = document.querySelector('svg');
+// let taipei = document.getElementById("TWN1166");
+// console.log(taipei);
+svg.addEventListener("click", function (e) {
+  const text = e.target.dataset['city']
+  title.textContent = text;
 
 });
+
 
 
